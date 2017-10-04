1e2ebff5ef0a8da0add110a18a1e69f8d576b27f
JosselynMAS <josh.jmas@gmail.com>
2017-10-04 17:47:41 +0200

    splash screen working

--- app/src/main/java/fr/wcs/viaferrata/HomeActivity.java
+++ app/src/main/java/fr/wcs/viaferrata/HomeActivity.java
@@ -1,9 +1,12 @@
 package fr.wcs.viaferrata;
+import android.app.ActionBar;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import com.google.firebase.database.DataSnapshot;
@@ -14,17 +17,35 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
+
 public class HomeActivity extends AppCompatActivity {
 
-    final private boolean hasFavorite = true;
+
     public static ArrayList<ViaFerrataModel> mViaFerrataList = new ArrayList<>();
     private static final String TAG = "HomeActivity";
+
+    /** Duration of wait for splash screen**/
+    private final int SPLASH_DISPLAY_LENGTH = 2500;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash);
+
+
+        //splash screen code
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the MapsActivity. */
+                Intent mainIntent = new Intent(HomeActivity.this,MapsActivity.class);
+                HomeActivity.this.startActivity(mainIntent);
+                HomeActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+        //end splash screen code
 
         // Retrieve all the viaFerrata from database and store it in global var
         FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -54,14 +75,15 @@ public class HomeActivity extends AppCompatActivity {
 
         // Everything below will be replaced by splashscreen
 
-        Button buttonRecherche=(Button)findViewById(R.id.buttonRecherche);
+       /* Button buttonRecherche=(Button)findViewById(R.id.buttonRecherche);
 
         buttonRecherche.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View view) {
                 Intent intentSearch=new Intent(HomeActivity.this,MapsActivity.class);
                 startActivity(intentSearch);
             }
-        });
+        });*/
         // Until here
+
     }
 }
