85da8269205f2c2e12309a5b915dca530a178e65
KekeTSC <kelian.daste31@gmail.com>
2017-10-16 15:54:05 +0200

    Ajout temp

--- app/src/main/java/fr/wcs/viaferrata/ViaActivity.java
+++ app/src/main/java/fr/wcs/viaferrata/ViaActivity.java
@@ -20,6 +20,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ShareActionProvider;
 import android.widget.Toast;
 
@@ -39,20 +40,32 @@ public class ViaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_via);
 
-        final BottomNavigationItemView favButton = (BottomNavigationItemView)findViewById(R.id.favButton);
-        final BottomNavigationItemView doneButton = (BottomNavigationItemView)findViewById(R.id.doneButton);
-
-            //Shared preferences
+        //Shared preferences
         Intent intentFav = getIntent();
         final ViaFerrataModel maviaferrata =  intentFav.getParcelableExtra("via");
         mySharedPref = getSharedPreferences("SP",MODE_PRIVATE);
 
+        final ImageButton backButton = (ImageButton) findViewById(R.id.backButton);
+        final ImageButton itineraryButton = (ImageButton) findViewById(R.id.itineraryButton);
+        final ImageButton favButton = (ImageButton) findViewById(R.id.favButton);
+        final ImageButton doneButton = (ImageButton) findViewById(R.id.doneButton);
+        final ImageButton shareButton = (ImageButton) findViewById(R.id.shareButton);
+
         final String favId = "Fav" + maviaferrata.getNom();
         final boolean isFavorite = mySharedPref.getBoolean(favId, false);
+        if (isFavorite) {
+            favButton.setImageResource(R.drawable.etoilechecked);
+        }else {
+            favButton.setImageResource(R.drawable.etoileunchecked);
+        }
 
         final String doneId = "Done" + maviaferrata.getNom();
         final boolean isDone = mySharedPref.getBoolean(doneId, false);
-        Log.i(TAG, "fav" +isFavorite);
+        if (isDone) {
+            doneButton.setImageResource(R.drawable.check);
+        }else {
+            doneButton.setImageResource(R.drawable.uncheck);
+        }
 
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
         // Set up the ViewPager with the sections adapter.
@@ -61,101 +74,70 @@ public class ViaActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
-        //TODO changer menu en bouton
-        //initialiser bouton favori et fait
-        if(isFavorite){
-            favButton.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
-        }else{
-            favButton.setBackgroundColor(getResources().getColor(R.color.transparent));
-
-        }
-
-        if(isDone){
-            doneButton.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
-        }else{
-            doneButton.setBackgroundColor(getResources().getColor(R.color.transparent));
-
-        }
-
-
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomButton);
-        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
-
-                    case R.id.backButton:
-                        Intent intent = new Intent(ViaActivity.this, MapsActivity.class);
-                        startActivity(intent);
-                        break;
-
-
-                    case R.id.favButton:
-                        final boolean isFavorite = mySharedPref.getBoolean(favId, false);
-                        boolean isFavNewValue = !isFavorite;
-                        String toastMessage;
-                        if(isFavorite){
-                            toastMessage = maviaferrata.getNom()+" "+"ne fait plus partie de vos favoris.";
-                        }else{
-                            toastMessage = maviaferrata.getNom()+" "+"a été ajoutée à vos favoris.";
-                        }
-                        Toast toastFavorite = Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_LONG);
-                        toastFavorite.show();
-                        mySharedPref.edit().putBoolean(favId, isFavNewValue).apply();
-                        final boolean isFavoriteNow = mySharedPref.getBoolean(favId, false);
-                        Log.i(TAG, "fav" +isFavoriteNow);
-
-                        if(isFavoriteNow){
-                            favButton.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
-                        }else{
-                            favButton.setBackgroundColor(getResources().getColor(R.color.transparent));
-                        }
-
-
-                        break;
-                    case R.id.doneButton:
-                        final boolean isDone = mySharedPref.getBoolean(doneId, false);
-                        boolean isDoneNewValue= !isDone;
-                        mySharedPref.edit().putBoolean(doneId, isDoneNewValue).apply();
-                        final boolean isDoneNow = mySharedPref.getBoolean(doneId, false);
-                        if(isDone){
-                            doneButton.setBackgroundColor(getResources().getColor(R.color.transparent));
-                            toastMessage = "Vous n'avez pas fait la via Ferrata"+" : "+maviaferrata.getNom()+".";
-                        }else{
-                            doneButton.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
-                            toastMessage = "Vous avez fait la via Ferrata"+" : "+maviaferrata.getNom()+".";
-                        }
-                        Toast.makeText(ViaActivity.this, toastMessage, Toast.LENGTH_LONG).show();
-
-                        break;
-                    case R.id.shareButton:
-
-                        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
-                        sharingIntent.setType("text/plain");
-                        String shareSubject = "Bonsoir";
-                        String shareBody = "\nTélécharge vite l'appli ViaFerrata et trouve ta prochaine sortie #sport et #nature !\n\n";
-                        shareBody = shareBody + "https://play.google.com/store/apps/details?id=Orion.Soft \n\n";
-                        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, shareSubject);
-                        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
-                        startActivity(Intent.createChooser(sharingIntent, "Share via"));
-
-                        break;
-                    case R.id.itineraryButton:
-                        Intent intentGM = new Intent(android.content.Intent.ACTION_VIEW,
-                                Uri.parse("http://maps.google.com/maps?saddr=&daddr=" + maviaferrata.getLatitude()
-                                        + ", " + maviaferrata.getLongitude()));
-                                Log.i(TAG, String.valueOf(maviaferrata.getLatitude()));
-                        startActivity(intentGM);
-
-                        break;
-                }
-                return false;
+            public void onClick(View view) {
+                Intent intent = new Intent(ViaActivity.this, MapsActivity.class);
+                startActivity(intent);
             }
         });
-
-
+        itineraryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentGM = new Intent(android.content.Intent.ACTION_VIEW,
+                        Uri.parse("http://maps.google.com/maps?saddr=&daddr="
+                                + maviaferrata.getLatitude()
+                                + ", " + maviaferrata.getLongitude()));
+                startActivity(intentGM);
+            }
+        });
+        favButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final boolean isFavorite = mySharedPref.getBoolean(favId, false);
+                boolean isFavNewValue = !isFavorite;
+                String toastMessage;
+                if(isFavorite){
+                    favButton.setImageResource(R.drawable.etoilechecked);
+                    toastMessage = maviaferrata.getNom()+" "+"ne fait plus partie de vos favoris.";
+                }else{
+                    favButton.setImageResource(R.drawable.etoileunchecked);
+                    toastMessage = maviaferrata.getNom()+" "+"a été ajoutée à vos favoris.";
+                }
+                Toast.makeText(ViaActivity.this, toastMessage, Toast.LENGTH_LONG).show();
+                mySharedPref.edit().putBoolean(favId, isFavNewValue).apply();
+            }
+        });
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final boolean isDone = mySharedPref.getBoolean(doneId, false);
+                boolean isDoneNewValue= !isDone;
+                String toastMessage;
+                if(isDone){
+                    doneButton.setImageResource(R.drawable.check);
+                    toastMessage = "Vous n'avez pas fait la via Ferrata"+" : "+maviaferrata.getNom()+".";
+                }else{
+                    doneButton.setImageResource(R.drawable.uncheck);
+                    toastMessage = "Vous avez fait la via Ferrata"+" : "+maviaferrata.getNom()+".";
+                }
+                mySharedPref.edit().putBoolean(doneId, isDoneNewValue).apply();
+                Toast.makeText(ViaActivity.this, toastMessage, Toast.LENGTH_LONG).show();
+            }
+        });
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                String shareSubject = "Bonsoir";
+                String shareBody = "\nTélécharge vite l'appli ViaFerrata et trouve ta prochaine sortie #sport et #nature !\n\n";
+                shareBody = shareBody + "https://play.google.com/store/apps/details?id=Orion.Soft \n\n";
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, shareSubject);
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(sharingIntent, "Share via"));
+            }
+        });
     }
 
     @Override
