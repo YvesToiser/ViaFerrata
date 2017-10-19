876ad3ded36499c1865070a7d8647bfeeec83074
KekeTSC <kelian.daste31@gmail.com>
2017-10-19 16:06:24 +0200

    fzijfziefji

--- app/src/main/java/fr/wcs/viaferrata/ViaActivity.java
+++ app/src/main/java/fr/wcs/viaferrata/ViaActivity.java
@@ -100,7 +100,6 @@ public class ViaActivity extends AppCompatActivity {
                         startActivity(intent);
                         break;
 
-
                     case R.id.favButton:
                         final boolean isFavorite = mySharedPref.getBoolean(favId, false);
                         boolean isFavNewValue = !isFavorite;
