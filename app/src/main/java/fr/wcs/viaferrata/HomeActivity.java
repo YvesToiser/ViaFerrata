b014ca093fa5eef9d1b8513d1b0f12da5804bbd2
KekeTSC <kelian.daste31@gmail.com>
2017-10-23 19:25:03 +0200

    Network et debut trier

--- app/src/main/java/fr/wcs/viaferrata/HomeActivity.java
+++ app/src/main/java/fr/wcs/viaferrata/HomeActivity.java
@@ -1,6 +1,7 @@
 package fr.wcs.viaferrata;
 import android.app.ActionBar;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,11 +21,13 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NetworkStateReceiver.NetworkStateReceiverListener {
 
+    private NetworkStateReceiver networkStateReceiver;
     public static ArrayList<ViaFerrataModel> mViaFerrataList = new ArrayList<>();
     public static SharedPreferences mySharedPref;
     private static final String TAG = "HomeActivity";
+    private boolean network;
 
     /** Duration of wait for splash screen**/
     private final int SPLASH_DISPLAY_LENGTH = 6000;
@@ -34,22 +38,32 @@ public class HomeActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.splash);
 
+        networkStateReceiver = new NetworkStateReceiver();
+        networkStateReceiver.addListener(this);
+        this.registerReceiver(networkStateReceiver, new IntentFilter(android.net.ConnectivityManager.CONNECTIVITY_ACTION));
 
         //splash screen code
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
-                /* Create an Intent that will start the MapsActivity. */
-                Intent mainIntent = new Intent(HomeActivity.this,MapsActivity.class);
-                HomeActivity.this.startActivity(mainIntent);
-                HomeActivity.this.finish();
+
+                if (network) {
+                    Intent mainIntent = new Intent(HomeActivity.this,MapsActivity.class);
+                    HomeActivity.this.startActivity(mainIntent);
+                    HomeActivity.this.finish();
+                }
+                else {
+                    Intent intent = new Intent(HomeActivity.this, MapsActivity.class);
+                    intent.putExtra("displayedChild", 1);
+                    intent.putExtra("checkMapList", true);
+                    startActivity(intent);
+                }
             }
         }, SPLASH_DISPLAY_LENGTH);
         //end splash screen code
 
-        //TO DO PREVOIR CAS OU  FIREBASE N'EST PAS ACCESSIBLE
-
         // Retrieve all the viaFerrata from database and store it in global var
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference viaFerrataRef = database.getReference("viaFerrata");
 
@@ -68,5 +82,17 @@ public class HomeActivity extends AppCompatActivity {
 
             }
         });
+
+    }
+
+    @Override
+    public void networkAvailable() {
+        network = true;
+    }
+
+    @Override
+    public void networkUnavailable() {
+        network = false;
+        Toast.makeText(this, "Vous n'avez pas de connection réseau, certaines fonctionnalité peuvent être limitées.", Toast.LENGTH_LONG).show();
     }
 }
